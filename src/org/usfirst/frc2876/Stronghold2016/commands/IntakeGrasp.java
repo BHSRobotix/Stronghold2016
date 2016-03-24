0a9c5ef8372bef8d40b0d51ccbdc818e2170d371
Red Laptop <bhsrobotix-programming@gmail.com>
2016-03-23 21:03:33 -0400

    Added test of limit switch for intake - still doesn't work.

--- src/org/usfirst/frc2876/Stronghold2016/commands/IntakeGrasp.java
+++ src/org/usfirst/frc2876/Stronghold2016/commands/IntakeGrasp.java
@@ -47,7 +47,7 @@ public class IntakeGrasp extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.intake.isLimitSwitchPressed();
     }
 
     // Called once after isFinished returns true
